c2d84f32ce8fccc386915cb9050a88b0cbd4936c
Juichi Hirao <hirao@theta-inc.co.jp>
2019-12-14 09:55:14 +0900

    collect_recover_javでimagebamのドメインの時にはタグなどが違うロジックで画像取得

--- scraping/collect_recover_jav.py
+++ scraping/collect_recover_jav.py
@@ -49,8 +49,10 @@ class CollectJav:
         start = idx = 1
 
         # javs = self.jav_dao.get_where_agreement('WHERE package IS NULL AND download_links IS NULL ORDER BY post_date')
-        javs = self.jav_dao.get_where_agreement('WHERE (package = \'\' or thumbnail = \'\') and created_at >= \'2019-04-01\' ORDER BY post_date')
-        # javs = self.jav_dao.get_where_agreement('WHERE id = 24583')
+        # javs = self.jav_dao.get_where_agreement('WHERE (package = \'\' or thumbnail = \'\') and created_at >= \'2019-12-01\' ORDER BY post_date')
+        # javs = self.jav_dao.get_where_agreement('WHERE id = 46078')
+        javs = self.jav_dao.get_where_agreement('WHERE thumbnail not like "%jpg%" and created_at >= "2019-10-01" and length(thumbnail) > 0')
+        # javs = self.jav_dao.get_where_agreement('WHERE ( post_date > "2019-11-04 13:41:00" and post_date <="2019-11-05 17:28:01") and is_selection >= 0 ')
 
         for jav in javs:
             print(str(idx) + '/' + str(len(javs)) + ' ' + jav.url + jav.productNumber)
@@ -75,10 +77,10 @@ class CollectJav:
                 if len(jav.package) <= 0:
                     print('  package image error')
 
-            if len(jav.thumbnail.strip()) <= 0:
-                jav.thumbnail = self.__download_thumbnails(page_data.thumbnail_links)
-                if len(jav.thumbnail) <= 0:
-                    print('  thumbnail image error')
+            # if len(jav.thumbnail.strip()) <= 0:
+            jav.thumbnail = self.__download_thumbnails(page_data.thumbnail_links)
+            if len(jav.thumbnail) <= 0:
+                print('  thumbnail image error')
 
             if jav.downloadLinks is not None and len(jav.downloadLinks.strip()) <= 0:
                 file_info_list = []
@@ -115,7 +117,8 @@ class CollectJav:
         is_hlink = False
         for a in entry.find_elements_by_tag_name('a'):
             contents_link = a.get_attribute('href')
-            if re.match('.*jpg$|.*jpeg$', contents_link):
+
+            if re.match('.*jpg$|.*jpeg$|.*imagebam.*', contents_link):
                 page_data.thumbnail_links.append(contents_link)
             if re.match('.*rapid.*', contents_link):
                 page_data.movie_links.append(contents_link)
@@ -181,9 +184,18 @@ class CollectJav:
         for link in links:
             self.driver.get(link)
 
+            m_imagebam = re.match('.*imagebam.*', link)
             # <a style="color: blue; font-size: 40px; text-decoration: underline; cursor: pointer;" class="continue">Continue to your image</a>
             try:
-                self.driver.find_element_by_class_name('continue').click()
+                if m_imagebam:
+                    a_links = self.driver.find_elements_by_tag_name('a')
+                    for a in a_links:
+                        if 'ontinue' in a.text:
+                            a.click()
+                            break
+                else:
+                    self.driver.find_element_by_class_name('continue').click()
+
                 sleep(1)
 
                 self.driver.switch_to.window(self.driver.window_handles[1])
@@ -194,15 +206,22 @@ class CollectJav:
                 for win in all_handles:
                     print('  win ' + str(win))
                     self.driver.switch_to.window(win)
-                    dl_filename = self.__download_image(self.driver, link)
+                    if m_imagebam:
+                        dl_filename = self.__download_image_imgbam(self.driver, link)
+                    else:
+                        dl_filename = self.__download_image(self.driver, link)
                     arr_dl_files.append(dl_filename)
                     break
 
             except:
                 print(sys.exc_info())
                 try:
-                    print('  not popup page')
-                    dl_filename = self.__download_image(self.driver, link)
+                    print('  not popup page   m_imagebam {}'.format(m_imagebam))
+                    if m_imagebam:
+                        dl_filename = self.__download_image_imgbam(self.driver, link)
+                    else:
+                        dl_filename = self.__download_image(self.driver, link)
+                    # dl_filename = self.__download_image(self.driver, link)
                     arr_dl_files.append(dl_filename)
                     break
 
@@ -222,6 +241,40 @@ class CollectJav:
             dl_filenames = ' '.join(arr_dl_files)
 
         return dl_filenames
+
+    def __download_image_imgbam(self, driver, link):
+
+        thumbnail_url = ''
+        try:
+            image_container = driver.find_element_by_class_name('image-container')
+        except selenium.common.exceptions.NoSuchElementException:
+            print(sys.exc_info())
+            basename = os.path.basename(link)
+            image_list = driver.find_elements_by_tag_name('img')
+            for img in image_list:
+                print('{} {}'.format(basename, str(img)))
+                img_src = img.get_attribute('src')
+                if basename in img_src:
+                    image_id = img
+                    break
+
+        if image_container is not None:
+            image_id = image_container.find_element_by_tag_name('img')
+            thumbnail_url = image_id.get_attribute('src')
+        else:
+            thumbnail_url = image_id.get_attribute('src')
+
+        filename = link[link.rfind("/") + 1:]
+        if not re.match('.*jpg$|.*jpeg$', filename):
+            filename = '{}.jpg'.format(filename)
+        pathname = os.path.join(self.store_path, filename)
+        # if os.path.isfile(pathname):
+        #     now_date = datetime.now().strftime('%Y-%m-%d-%H%M%S')
+        #     pathname = os.path.join(self.store_path, now_date + '_' + filename)
+        print('    imgaebam img_th ' + filename + ' ' + thumbnail_url)
+        urllib.request.urlretrieve(thumbnail_url, pathname)
+
+        return filename
 
     def __download_image(self, driver, link):
 
